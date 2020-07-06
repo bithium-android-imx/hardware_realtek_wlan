cd815ded249f858ed3fdd1828452844e219ba0fe
Bithium S.A <development@bithium.com>
2020-07-06 13:33:21 +0000

    CHG: Generate wpa_supplicant.conf for realtek wifi chipsets.

--- Android.mk
+++ Android.mk
@@ -1,3 +1,8 @@
 ifeq ($(BOARD_WIFI_VENDOR), realtek)
 	include $(call all-subdir-makefiles)
+
+	WIFI_DRIVER_SOCKET_IFACE := wlan0
+	ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_8_X)
+	  include external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
+	endif
 endif
